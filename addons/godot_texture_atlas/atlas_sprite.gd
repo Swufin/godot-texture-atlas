2b50781d881cb8422410f0dcabda295c81d84f53
Swufin <110313086+Swufin@users.noreply.github.com>
2026-02-02 20:45:08 -0800

    Update atlas_sprite.gd

--- addons/godot_texture_atlas/atlas_sprite.gd
+++ addons/godot_texture_atlas/atlas_sprite.gd
@@ -1,8 +1,10 @@
 @icon("res://addons/godot_texture_atlas/icon_atlas_sprite.svg")
 @tool
-class_name AtlasSprite extends Node2D
-## A sprite that uses Adobe Animate's sprite atlases to render.
+## Plays Adobe Animate sprite atlases in Godot.
+class_name AtlasSprite
+extends Node2D
 
+# Base key mapping from Adobe Animate JSON
 const NAMES_BASE = {
 	"ANIMATION": "AN",
 	"SYMBOL_DICTIONARY": "SD",
@@ -28,184 +30,168 @@ const NAMES_BASE = {
 	"framerate": "FRT"
 }
 
-# empty is defaulted to timeline
-@export var symbol: String = "": ## The current symbol
+# Inspector properties
+@export var animations: Array[AtlasAnimInfo] = [] ## Array of animations this sprite can play
+@export var animation: int = 0: ## Active animation index
 	set(value):
-		symbol = value
-		
-		if symbols.is_empty() == false:
-			timeline_length = get_timeline_length(get_layers())
-		
+		animation = value
+		_load_current_animation()
+
+@export var frame: int = 0: ## Current frame of the active animation
+	set(value):
+		frame = value
 		queue_redraw()
+		
+@export var fps: int = 24 ## Frames per second
+@export_tool_button("Reload Atlas") var reload_atlas = _load_current_animation ## Reload atlas button
+@export_tool_button("Create Animation Player") var create_animplayer = _create_animation_player ## Create animations in child AnimationPlayer
 
-@export var cur_frame: int = 0: ## The current frame of the [member symbol]'s timeline.
-	set(value):
-		cur_frame = value
-		queue_redraw()
-
-@export var animation_json: JSON: ## The animation [JSON] file of your sprite atlas
-	set(value):
-		animation_json = value
-		notify_property_list_changed()
-		_load_atlas()
-
-@export_tool_button("Reload Atlas") var reload_atlas = _load_atlas
-
-@export_group("Animation Player")
-
-## The [AnimationPlayer] that will get the animations placed inside of. [br] See [member ap_animations] for more info.
-@export var animation_player_node: AnimationPlayer:
-	set(value):
-		animation_player_node = value
-		notify_property_list_changed()
-
-@export var ap_fps: int = 24 ## The FPS the current animation will play at.
-
-## List of animations that will be added into the [member animation_player_node]. [br] [br]
-## [b]How to use?[/b][br]
-## For each symbol you want to use, create an [AtlasAnimInfo] and enter the symbol's name into [member AtlasAnimInfo.symbol_name]. [br] [br]
-## [i](This must be the full path to the symbol in the AE library, including any folders and such.)[/i] [br] [br]
-## Once you've selected all the symbols you want to use, press the [code]Create Animation[/code] button and the animations will be added to your [member animation_player_node].
-@export var ap_animations: Array[AtlasAnimInfo] = []
-@export_tool_button("Create Animation") var ap_create_anim_button = _create_animation
-
-
-# duplicate json resource that we can edit
+# Internal variables
 var _animation_json: Dictionary
+var spritemap_tex: Texture2D
+var spritemap_json: JSON
+var symbols: Dictionary[String, Array] = {}
+var limbs: Dictionary[String, Rect2i] = {}
+var NAMES: Dictionary
+var timeline_length: int = 0
+var is_optimized: bool = false
+var _current_animation: AtlasAnimInfo = null
+var _loaded_json: JSON = null
 
-var NAMES:Dictionary
-var timeline_length:int = 0
-var is_optimized:bool = false
+func _ready() -> void:
+	_update_current_animation()
+	_load_current_animation()
 
-var spritemap_tex:Texture2D
-var spritemap_json:JSON
-
-var limbs:Dictionary[String, Dictionary] = {}
-var symbols:Dictionary[String, Array] = {}
-
-#func _ready() -> void:
-	#_load_atlas()
-
-func _validate_property(property: Dictionary) -> void:
-	if property.name == "symbol":
-		property.hint = PROPERTY_HINT_ENUM
-		property.hint_string = ",".join(symbols.keys())
-	elif property.name.begins_with("ap_"):
-		if !animation_player_node:
-			property.usage = PROPERTY_USAGE_NONE
-
-func _draw() -> void:
-	if !symbols.is_empty():
-		_draw_timeline(get_layers(), cur_frame)
-
-func _load_atlas() -> void:
-	if !animation_json:
-		_animation_json.clear()
-		timeline_length = 0
-		
-		spritemap_tex = null
-		spritemap_json = null
-		
-		limbs.clear()
-		symbols.clear()
-		queue_redraw()
+# Set active animation by index
+func set_current_anim(index: int) -> void:
+	if index < 0 or index >= animations.size():
+		push_warning("Invalid animation index ", index)
 		return
+	animation = index
+	_update_current_animation()
+	_load_current_animation(false)
+	queue_redraw()
+
+# Update reference to current animation
+func _update_current_animation() -> void:
+	if animations.size() == 0:
+		_current_animation = null
+	else:
+		_current_animation = animations[animation]
+
+# Load JSON and texture atlas for current animation
+func _load_current_animation(reset_frame: bool=true) -> void:
+	_update_current_animation()
 	
-	var dir = animation_json.resource_path.get_base_dir()
+	if _current_animation == null or _current_animation.animation_json == null:
+		return
+
+	if _current_animation.animation_json == _loaded_json:
+		pass
+
+	var dir = _current_animation.animation_json.resource_path.get_base_dir()
 	spritemap_tex = load(dir.path_join("spritemap1.png"))
 	spritemap_json = load(dir.path_join("spritemap1.json"))
-	
-	_animation_json = animation_json.data.duplicate(true)
+	_animation_json = _current_animation.animation_json.data.duplicate(true)
 	
 	is_optimized = _animation_json.has("AN")
-	
 	NAMES = NAMES_BASE.duplicate()
 	limbs.clear()
 	symbols.clear()
 	
-	if !is_optimized:
+	if not is_optimized:
 		for key in NAMES:
 			NAMES[key] = key
 	
+	# Build limbs from texture atlas
 	for _sprite in spritemap_json.data["ATLAS"]["SPRITES"]:
-		var sprite = _sprite["SPRITE"] # i dont know why these are in their own dict
-		limbs[sprite["name"]] = {
-			"rect": Rect2(int(sprite["x"]), int(sprite["y"]), int(sprite["w"]), int(sprite["h"])),
-			"rotated": sprite.has("rotated") and sprite["rotated"],
-			"source_size": Vector2(int(sprite.get("sourceSize", {}).get("w", sprite["w"])), int(sprite.get("sourceSize", {}).get("h", sprite["h"]))),
-			"spriteSourceSize": Vector2(int(sprite.get("spriteSourceSize", {}).get("x", 0)), int(sprite.get("spriteSourceSize", {}).get("y", 0)))
-		}
+		var sprite = _sprite["SPRITE"]
+		limbs[sprite["name"]] = Rect2i(int(sprite["x"]), int(sprite["y"]), int(sprite["w"]), int(sprite["h"]))
 	
+	# Build symbols from JSON
 	if _animation_json.has(NAMES["SYMBOL_DICTIONARY"]):
 		for symbol_data in _animation_json[NAMES["SYMBOL_DICTIONARY"]][NAMES["Symbols"]]:
 			symbols[symbol_data[NAMES["SYMBOL_name"]]] = symbol_data[NAMES["TIMELINE"]][NAMES["LAYERS"]]
 			symbols[symbol_data[NAMES["SYMBOL_name"]]].reverse()
 	
-	# lets just hope no one names their symbol this lol
+	# Ensure top-level timeline exists
 	symbols["_top"] = _animation_json[NAMES["ANIMATION"]][NAMES["TIMELINE"]][NAMES["LAYERS"]]
 	symbols["_top"].reverse()
 	
-	timeline_length = get_timeline_length(get_layers())
+	timeline_length = get_timeline_length(_get_layers())
 	
+	if reset_frame:
+		frame = 0
+	
+	_loaded_json = _current_animation.animation_json
 	queue_redraw()
 
-func get_layers() -> Array:
-	if symbol == "" || !symbols.has(symbol):
-		return symbols["_top"]
-	else:
-		return symbols[symbol]
+# Draw
+func _draw() -> void:
+	if symbols.size() > 0:
+		_draw_timeline(_get_layers(), frame)
 
-func get_timeline_length(layers:Array) -> int:
+# Get layers for current animation
+func _get_layers() -> Array:
+	if _current_animation == null:
+		return symbols["_top"]
+	return symbols["_top"]
+
+# Compute total timeline length
+func get_timeline_length(layers: Array) -> int:
 	var longest_length = 0
 	for layer in layers:
-		var total_duration:int = 0
-		
-		for frame in layer[NAMES["Frames"]]:
-			total_duration += frame[NAMES["duration"]]
-		
+		var total_duration: int = 0
+		for frame_data in layer[NAMES["Frames"]]:
+			total_duration += frame_data[NAMES["duration"]]
 		if total_duration > longest_length:
 			longest_length = total_duration
-	
 	return longest_length
 
-func _draw_timeline(layers:Array, starting_frame:int, transformation:Transform2D = Transform2D()) -> void:
+# Get frame and its index at a specific timeline frame
+func _get_frame_and_index(target_frame: int, frames: Array) -> Dictionary:
+	var accumulated_frames = 0
+	for i in range(frames.size()):
+		accumulated_frames += frames[i][NAMES["duration"]]
+		if target_frame <= accumulated_frames:
+			return {"frame": frames[i], "index": i}
+	return {"frame": {}, "index": 0}
+
+# Draw timeline recursively
+func _draw_timeline(layers: Array, starting_frame: int, transformation: Transform2D = Transform2D()) -> void:
 	for layer in layers:
-		var frame = get_index_at_frame(starting_frame, layer[NAMES["Frames"]])
-		if frame.is_empty(): continue
-		for _element:Dictionary in frame[NAMES["elements"]]:
-			var type = _element.keys()[0]
-			var element = _element[type]
-			var transform_2d = transformation * m3d_to_transform2d(element[NAMES["Matrix3D"]])
+		var frame_data = _get_frame_and_index(starting_frame, layer[NAMES["Frames"]])
+		var frame_dict = frame_data["frame"]
+		var frame_index = frame_data["index"]
+		
+		if frame_dict.size() == 0:
+			continue
+		
+		for element_type in frame_dict[NAMES["elements"]]:
+			var type = element_type.keys()[0]
+			var element = element_type[type]
+			var transform_2d = transformation * _m3d_to_transform2d(element[NAMES["Matrix3D"]])
+			
 			match type:
 				"ATLAS_SPRITE_instance", "ASI":
 					var limb = limbs[element[NAMES["name"]]]
-					var src = limb["rect"]
-					var offset = limb["spriteSourceSize"]
-					var draw_size = src.size
-					var local_transform = Transform2D()
-					
-					if limb["rotated"]:
-						var rotated_offset = Vector2(offset.y, draw_size.x - offset.x)
-						local_transform = Transform2D(-PI/2, rotated_offset)
-					else:
-						local_transform.origin = -offset
-					
-					draw_set_transform_matrix(transform_2d * local_transform)
-					draw_texture_rect_region(spritemap_tex, Rect2(Vector2.ZERO, draw_size), src)
+					draw_set_transform_matrix(transform_2d)
+					draw_texture_rect_region(spritemap_tex, Rect2i(0, 0, limb.size.x, limb.size.y), limb)
 				"SYMBOL_Instance", "SI":
 					var new_starting_frame = 0
 					if element[NAMES["symbolType"]] == NAMES["movieclip"]:
-						new_starting_frame = cur_frame
+						new_starting_frame = frame_index
 					else:
 						new_starting_frame = element[NAMES["firstFrame"]] + 1
 					_draw_timeline(symbols[element[NAMES["SYMBOL_name"]]], new_starting_frame, transform_2d)
 				_:
 					push_warning("Unsupported type ", type, "!")
 
-func m3d_to_transform2d(matrix) -> Transform2D:
-	var x_axis:Vector2
-	var y_axis:Vector2
-	var translation:Vector2
+# Convert 3D matrix to 2D transform
+func _m3d_to_transform2d(matrix) -> Transform2D:
+	var x_axis: Vector2
+	var y_axis: Vector2
+	var translation: Vector2
 	
 	if is_optimized:
 		x_axis = Vector2(matrix[0], matrix[1])
@@ -215,54 +201,49 @@ func m3d_to_transform2d(matrix) -> Transform2D:
 		x_axis = Vector2(matrix["m00"], matrix["m01"])
 		y_axis = Vector2(matrix["m10"], matrix["m11"])
 		translation = Vector2(matrix["m30"], matrix["m31"])
-
+	
 	return Transform2D(x_axis, y_axis, translation)
 
-func get_index_at_frame(target_frame:int, frames:Array) -> Dictionary:
-	var accumulated_frames = 0
-	for frame in frames:
-		accumulated_frames += frame[NAMES["duration"]]
-		if target_frame <= accumulated_frames:
-			return frame
-	
-	return {}
-
-func _create_animation() -> void:
-	animation_player_node = $AnimationPlayer if has_node("AnimationPlayer") else null
-	if animation_player_node == null:
-		push_error("The `animation_player_node` must be set!")
+# Create AnimationPlayer tracks for all animations
+func _create_animation_player() -> void:
+	var anim_player: AnimationPlayer = $AnimationPlayer if has_node("AnimationPlayer") else null
+	if anim_player == null:
+		push_error("Must have an AnimationPlayer as a child!")
 		return
-
-	for anim_info in ap_animations:
-		if !symbols.has(anim_info.symbol_name): continue
+	
+	for anim_info in animations:
+		if anim_info.symbol_name == "":
+			continue
 		
-		var layers = symbols[anim_info.symbol_name]
+		var layers = symbols.get(anim_info.symbol_name, symbols["_top"])
 		var total_frames = get_timeline_length(layers)
 		if total_frames <= 0:
 			continue
-
+		
 		var anim = Animation.new()
-		anim.length = float(total_frames) / ap_fps
+		anim.length = float(total_frames) / fps
 		anim.loop_mode = anim_info.loop_mode
 		
-		var track_symbol = anim.add_track(Animation.TYPE_VALUE)
-		anim.track_set_path(track_symbol, ".:symbol")
-		anim.value_track_set_update_mode(track_symbol, Animation.UPDATE_DISCRETE)
-		anim.track_insert_key(track_symbol, 0.0, anim_info.symbol_name)
+		# Track animation index
+		var track_anim_index = anim.add_track(Animation.TYPE_VALUE)
+		anim.track_set_path(track_anim_index, ".:animation")
+		anim.value_track_set_update_mode(track_anim_index, Animation.UPDATE_DISCRETE)
+		anim.track_insert_key(track_anim_index, 0.0, animations.find(anim_info))
 		
+		# Track frame
 		var track_frame = anim.add_track(Animation.TYPE_VALUE)
-		anim.track_set_path(track_frame, ".:cur_frame")
-		
+		anim.track_set_path(track_frame, ".:frame")
 		anim.track_insert_key(track_frame, 0, 0)
 		anim.track_insert_key(track_frame, anim.length, total_frames)
 		
+		# Animation name
 		var anim_name = anim_info.symbol_name.replace("/", "_").replace(" ", "_")
 		
-		var lib:AnimationLibrary
-		if not animation_player_node.has_animation_library("AtlasSymbols"):
+		var lib: AnimationLibrary
+		if not anim_player.has_animation_library("AtlasSymbols"):
 			lib = AnimationLibrary.new()
-			animation_player_node.add_animation_library("AtlasSymbols", lib)
+			anim_player.add_animation_library("AtlasSymbols", lib)
 		else:
-			lib = animation_player_node.get_animation_library("AtlasSymbols")
+			lib = anim_player.get_animation_library("AtlasSymbols")
 		
 		lib.add_animation(anim_name, anim)
